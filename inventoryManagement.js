845b5a8eb269e4f285e9d6f7a9096574951b663a
Edwin Ngengi <edwinngengi@gmail.com>
2026-04-05 21:19:15 +0300

    Finished lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,12 +1,18 @@
-// Write your code here
+let products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+function logFirstProduct() {
+  console.log(products[0]);
+}
 
+function addProduct(productName) {
+  products.push(productName);
+}
 
-// Export the necessary parts for testing
-module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
-};
+function updateProductName(position, newName) {
+  products[position] = newName;
+}
+
+function removeLastProduct() {
+  products.pop();
+}
+console.log("Initial products:", products);
